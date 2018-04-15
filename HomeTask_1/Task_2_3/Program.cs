43ba9dd29746a9c716411fd9b0d7b69f723d4531
ShemetViktoriya <shemetvic@ukr.net>
2018-04-15 22:16:06 +0300

    HomeTask_1

---
+++ HomeTask_1/Task_2_3/Program.cs
@@ -0,0 +1,78 @@
+﻿#region Hometask_01_2_3
+//
+// Author: Shemet Viktoriya
+// Description: The user enters month number
+// if month 1,2,12 display "Winter"; 3,4,5 - "Spring";
+// 6-8 - "Summer"; 9-11 - "Autumn". In any other case - 
+// "There is no such month on this planet"
+//
+#endregion
+using System;
+namespace Task_2_3
+{
+    enum Season
+    {
+        Winter,
+        Spring,
+        Summer,
+        Autumn,
+    }
+    class Program
+    {
+        /// <summary>
+        /// Read user input and return number of month in right case  
+        /// </summary>
+        /// <returns>number of month or byte.MaxValue</returns>
+        static byte ReadMonth()
+        {
+            string enter;
+            Console.Write("Enter month number: ");
+            enter = Console.ReadLine();
+            if (byte.TryParse(enter, out byte numberMonth))
+            {
+                return byte.Parse(enter);
+            }
+            return byte.MaxValue;
+        }
+
+        /// <summary>
+        /// This Method Display season by the entered month
+        /// </summary>
+        /// <param name="numberOfMonth"></param>
+        static void DisplaySeason(byte numberOfMonth)
+        {
+            switch (numberOfMonth)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine(Season.Winter);
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine(Season.Spring);
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine(Season.Summer);
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine(Season.Autumn);
+                    break;
+                default:
+                    Console.WriteLine("There is no such month on this planet.");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            byte month = ReadMonth();
+            DisplaySeason(month);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeTask_1/Task_3_6/Program.cs
@@ -0,0 +1,58 @@
+﻿#region Hometask_01_3_6
+//
+// Author: Shemet Viktoriya
+// Description: Create 5 random integer numbers,
+// calculate their average value and inform the user.
+//
+#endregion
+using System;
+using System.Linq;
+namespace Task_3_6
+{
+    class Program
+    {
+        /// <summary>
+        /// Create random numbers in Array, between 0 and 100
+        /// </summary>
+        /// <param name="array"></param>
+        static void CreateRandomNumbers(int[] array)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+            }
+        }
+
+        /// <summary>
+        /// Display elements of 1-dimension array
+        /// </summary>
+        /// <param name="array"></param>
+        static void DisplayArray(int[] array)
+        {
+            Console.WriteLine("Array:");
+            foreach (int item in array)
+            {
+                Console.Write(item + "\t");
+            }
+        }
+
+        /// <summary>
+        /// Calculate average of elements of array
+        /// </summary>
+        /// <param name="array"></param>
+        static double CalculateAverage(int[] array)
+        {
+            return array.Average();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] randomArray = new int[5]; ;
+            CreateRandomNumbers(randomArray);
+            DisplayArray(randomArray);
+            Console.WriteLine($"\nAverage of elements = {CalculateAverage(randomArray)}");
+            Console.ReadKey();
+        }
+    }
+}
