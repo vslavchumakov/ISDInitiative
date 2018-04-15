7582650bfee71e9f86c6928ef1f797a269261d98
vslavchumakov <vslavchumakov@gmail.com>
2018-04-15 13:16:49 +0300

    add structure ISDInitiative

---
+++ HomeTask_2/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_2.Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ HomeTask_2/Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_2.Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep();
+        }
+    }
+}
