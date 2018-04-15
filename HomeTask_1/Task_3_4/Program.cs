1b601c141284acede1c01e07f9ef8bb3ea32e074
Irina <cg.irenecooper@gmail.com>
2018-04-15 15:32:48 +0300

    add_tasks

---
+++ HomeTask_1/Task_3_4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_4
+{
+    /// <summary>
+    /// 3.4. Выведите все точные квадраты натуральных чисел,
+    /// которые (квадраты) меньше заданного числа N.
+    /// (натуральные - это числа, которые мы используем при счёте: 1, 2, 3 и т.д.)
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// запрашиваем у пользователя число
+        /// </summary>
+        /// <returns>int введенное число</returns>
+        private static int GetNumber()
+        {
+            string enter;
+            while (true)
+            {
+                Console.Write("ВВедите число: ");
+                enter = Console.ReadLine();
+                if ((int.TryParse(enter, out int number) && number > 0))
+                {
+                    return number;
+                }
+            };
+        }
+
+        /// <summary>
+        /// Выводим квадраты натуральных чисел от 1 до n
+        /// </summary>
+        /// <param name="n">последнее натуральное число вывода</param>
+        private static void ShowSquares(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("number : " + i + "    square: " + Math.Pow(i, 2));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ShowSquares(GetNumber());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeTask_1/Task_3_8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_8
+{
+    /// <summary>
+    /// 3.8. Напишите программу, которая будет «спрашивать» правильный пароль,
+    /// до тех пор, пока он не будет введен. Правильный пароль пусть будет «root».
+    /// Если пароль не верный, программа должна сказать "Неверный пароль!"
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// пароль
+        /// </summary>
+        private static string _truePassword = "root";
+
+        /// <summary>
+        /// запрашиваем пароль
+        /// </summary>
+        private static void GetPassword()
+        {
+            string password;
+            do
+            {
+                Console.Write("Введите пароль: ");
+                password = Console.ReadLine();
+            } while (_truePassword != password);
+            Console.WriteLine("Привет админ!");
+        }
+
+        static void Main(string[] args)
+        {
+            GetPassword();
+        }
+    }
+}
