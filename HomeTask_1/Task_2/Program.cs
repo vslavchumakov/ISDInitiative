2a61b0d8ae9e89399063402a1a55fd26e2d2fe9c
Irina <cg.irenecooper@gmail.com>
2018-04-15 14:38:30 +0300

    add_task

--- HomeTask_1/Task_2/Program.cs
+++ HomeTask_1/Task_2/Program.cs
@@ -8,8 +8,28 @@ namespace HomeTask_1.Task_2
 {
     class Program
     {
+        /// <summary>
+        /// запрашиваем у пользователя число
+        /// </summary>
+        /// <returns>int введенное число</returns>
+        private static int ReadNumber()
+        {
+            string enter;
+            do
+            {
+                Console.Write("ВВедите число: ");
+                enter = Console.ReadLine();
+
+            } while (!int.TryParse(enter, out int number));
+            return int.Parse(enter);
+        }
+
         static void Main(string[] args)
         {
+            int number1 = ReadNumber();
+            Console.WriteLine();
+            int number2 = ReadNumber();
+            Console.WriteLine(number1 > number2 ? number1 : number2);
         }
     }
 }
