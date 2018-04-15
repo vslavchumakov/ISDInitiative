2adb1dd3824ea9e7aa0097e10a989a6d92108fc1
vslavchumakov <vslavchumakov@gmail.com>
2018-04-15 16:44:00 +0300

    add Task_2_4

---
+++ HomeTask_1/Task_2_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_4
+{
+    /// <summary>
+    /// Пользователь вводит число 1 или 0 с помощью одной строки кода вывести на экран «Хорошо»
+    /// если пользователь ввел 1 или «Плохо» если пользователь ввел 0 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число 0 или 1:");
+
+            int input = Int32.Parse(Console.ReadLine());
+
+            string output = input == 1 ? "Хорошо" : "Плохо";
+
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+    }
+}
