ce2bad68004c4e6ddfb023a1b37be6d873927a45
Oleg <olegpetrushov@gmail.com>
2018-04-15 23:24:33 +0300

    task 2 2
    added task 2.2

---
+++ Task_2_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            /// <summary>
+            /// Запрашиваем у пользователя строку на ввод
+            /// </summary>
+            Console.WriteLine("Введите мяу или гав:"); Console.Write("\n->");
+            userInput = Console.ReadLine();
+            userInput = userInput.ToLower();
+            userInput = userInput.Trim();
+            /// <summary>
+            /// Обрабатываем строку и выводим соответствующий ответ
+            /// </summary>
+            if (userInput == "мяу")
+                Console.WriteLine("Покорми кота");
+            if (userInput == "гав")
+                Console.WriteLine("Погуляй с собакой");
+            else
+                Console.WriteLine("Вы ввели: {0}", userInput);
+            Console.ReadKey();
+        }
+    }
+}
