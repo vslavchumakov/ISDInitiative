8bf7fd47306131208052f591da8fbc3281e5113c
vchumakov <vchumakov@playtika.com>
2018-04-17 17:38:51 +0300

    add HomeTask_2 Task_3

---
+++ HomeTask_2/Task_3/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Book
+    {
+        public Author Author { get; private set; }
+
+        public Content Content { get; private set; }
+
+        public Title Title { get; private set; }
+
+        public Book()
+        {
+            Author = new Author();
+            Content = new Content();
+            Title = new Title();
+        }
+
+        public void AddTitle(string title)
+        {
+            Title.title = title;
+        }
+
+        public void AddContent(string content)
+        {
+            Content.content = content;
+        }
+
+        public void AddAuthor(string author)
+        {
+            Author.author = author;
+        }
+
+        public void Show()
+        {
+            Title.Show();
+            Content.Show();
+            Author.Show();
+        }
+    }
+}
---
+++ HomeTask_2/Task_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = "This new edition of Pro C# 5.0 and the .NET 4.5 Platform" +
+            "\n has been completely revised and rewritten to reflect the latest changes" +
+            "\n to the C# language specification and new advances in the .NET Framework.";
+
+            string title = "Pro C# 5.0 and the .NET 4.5 Framework, 6th edition";
+
+            string author = "Andrew Troelsen";
+
+            Book book = new Book();
+
+            book.AddTitle(title);
+            book.AddAuthor(author);
+            book.AddContent(content);
+
+            book.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
