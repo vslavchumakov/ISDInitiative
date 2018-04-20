7bf93db1df9a614d9f99cd41d1ed81b39b8d3c3f
Irina <cg.irenecooper@gmail.com>
2018-04-20 21:08:29 +0300

    hometask2, task 1

---
+++ HomeTask_2/Task_1/Adress.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2_1
+{
+    /// <summary>
+    /// класс адрес
+    /// </summary>
+    class Address
+    {
+        /// <summary>
+        /// св-во индекс
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// св-во страна
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// св-во город
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// св-во улица
+        /// </summary>
+        public string Street { get; set; }
+
+        /// <summary>
+        /// св-во дом
+        /// </summary>
+        public string House { get; set; }
+
+        /// <summary>
+        /// св-во квартира
+        /// </summary>
+        public string Apartment { get; set; }
+
+        /// <summary>
+        /// переопределение метода ToString(), наследуемого от базового класса object
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder address = new StringBuilder();
+            if (Index != 0)
+            {
+                address.Append("Индекс " + Index + "\n");
+            }
+            if (!String.IsNullOrEmpty(Country))
+            {
+                address.Append("Страна " + Country + "\n");
+            }
+            if (!String.IsNullOrEmpty(City))
+            {
+                address.Append("Город " + City + "\n");
+            }
+            if (!String.IsNullOrEmpty(Street))
+            {
+                address.Append("Улица " + Street + "\n");
+            }
+            if (!String.IsNullOrEmpty(House))
+            {
+                address.Append("Дом " + House + "\n");
+            }
+            if (!String.IsNullOrEmpty(Apartment))
+            {
+                address.Append("Квартира " + Apartment + "\n");
+            }
+            return address.ToString();
+        }
+
+    }
+}
--- HomeTask_2/Task_1/Program.cs
+++ HomeTask_2/Task_1/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task_1
+namespace HomeWork_2_1
 {
+    /// <summary>
+    /// Создать класс с именем Address.
+    //В теле класса требуется создать поля: index, country, city, street, house, apartment.Для каждого поля, создать свойство с двумя методами доступа.
+    //Создать экземпляр класса Address.
+    //В поля экземпляра записать информацию о почтовом адресе.
+    //Выведите на экран значения полей, описывающих адрес.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //тестовые экземпляры
+            Address address1 = new Address { Index = 49089, Country = "Украина", City = "Днепр", Street = "Кирова", House = "15a", Apartment = "12б" };
+            Console.WriteLine(address1);
+
+            Address address2 = new Address { Index = 49018, Country = "Украина", City = "Днепр", Street = "Мониторная", House = "2", Apartment = "" };
+            Console.WriteLine(address2);
+
+            Console.ReadKey();
         }
     }
 }
