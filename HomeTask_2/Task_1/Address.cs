3e9243875a86af96b2ffe0c97505380e551392ef
Oleg <olegpetrushov@gmail.com>
2018-04-20 20:11:55 +0300

    edit
    edited

---
+++ HomeTask_2/Task_1/Address.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Address
+    {
+        private int index;
+        public int Index
+        {
+            get
+            {
+                return this.index;
+            }
+            set
+            {
+                this.index = value;
+            }
+        }
+        string country;
+        public string Country
+        {
+            get
+            {
+                return this.country;
+            }
+            set
+            {
+                this.country = value;
+            }
+        }
+        string city;
+        public string City
+        {
+            get
+            {
+                return this.city;
+            }
+            set
+            {
+                this.city = value;
+            }
+        }
+        string street;
+        public string Street
+        {
+            get
+            {
+                return this.street;
+            }
+            set
+            {
+                this.street = value;
+            }
+        }
+        int house;
+        public int House
+        {
+            get
+            {
+                return this.house;
+            }
+            set
+            {
+                this.house = value;
+            }
+        }
+        int apartment;
+        public int Apartament
+        {
+            get
+            {
+                return this.apartment;
+            }
+            set
+            {
+                this.apartment = value;
+            }
+        }
+        public void Print() => Console.WriteLine("Index:\t\t{0}" +
+                                               "\nCountry:\t{1}" +
+                                                "\nCity:\t\t{2}" +
+                                                "\nStreet:\t\t{3}" +
+                                                "\nHouse:\t\t{4}" +
+                                                "\nApartment:\t{5}\n\n",
+            index,country, city, street, house, apartment);
+
+    }
+}
--- HomeTask_2/Task_1/Program.cs
+++ HomeTask_2/Task_1/Program.cs
@@ -10,6 +10,11 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Address address1 = new Address { Index = 49000, Country = "Украина", City = "Днепр", Street = "Набережная", House = 121, Apartament = 20 };
+            address1.Print();
+            Address address2 = new Address { Index = 49018, Country = "Украина", City = "Днепр", Street = "Яворницкого", House = 33, Apartament = 11 };
+            address2.Print();
+            Console.ReadKey();
         }
     }
 }
