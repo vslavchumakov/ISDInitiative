46585389df3fdf864a527e5fbfb344e0101b4478
Irina <cg.irenecooper@gmail.com>
2018-04-21 09:20:29 +0300

    delete double task

--- HomeTask_2/Task_1/Program.cs
+++ HomeTask_2/Task_1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork_2_1
+namespace Task_1
 {
     /// <summary>
     /// Создать класс с именем Address.
@@ -17,20 +17,11 @@ namespace HomeWork_2_1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Address address1 = new Address { Index = 49000, Country = "Украина", City = "Днепр", Street = "Набережная", House = 121, Apartament = 20 };
             address1.Print();
             Address address2 = new Address { Index = 49018, Country = "Украина", City = "Днепр", Street = "Яворницкого", House = 33, Apartament = 11 };
             address2.Print();
-=======
-            //тестовые экземпляры
-            Address address1 = new Address { Index = 49089, Country = "Украина", City = "Днепр", Street = "Кирова", House = "15a", Apartment = "12б" };
-            Console.WriteLine(address1);
-
-            Address address2 = new Address { Index = 49018, Country = "Украина", City = "Днепр", Street = "Мониторная", House = "2", Apartment = "" };
-            Console.WriteLine(address2);
-
->>>>>>> ba38558c63163afb9b871d84acf8b51e62981e4e
+            
             Console.ReadKey();
         }
     }
