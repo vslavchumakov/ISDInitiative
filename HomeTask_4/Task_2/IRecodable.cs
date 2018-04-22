dd54f810bd54319597d8447905348b6fd5b4ffc1
ShemetViktoriya <shemetvic@ukr.net>
2018-04-22 03:07:37 +0300

    Add HomeTask_4

--- HomeTask_4/Task_2/IRecodable.cs
+++ HomeTask_4/Task_2/IRecodable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_2
+﻿namespace Task_2
 {
     // Declare interface:
     interface IRecodable
