54097b662b752d094265532ba7bf1ee603f4a0ee
vslavchumakov <vslavchumakov@gmail.com>
2018-04-24 07:22:55 +0300

    add HomeTask_6/Task_1

---
+++ HomeTask_6/Task_1/Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_1
+{
+    public struct Notebook
+    {
+        private string model;
+        private string manufacturer;
+        private int price;
+
+        public Notebook(string model, string manufacturer, int price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Notebook");
+            Console.WriteLine("Model: {0}", model);
+            Console.WriteLine("Manufacturer: {0}", manufacturer);
+            Console.WriteLine("Price: {0} UAH", price);
+            Console.WriteLine("____________________");
+        }
+    }
+}
---
+++ HomeTask_6/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Notebook> notebooks = new List<Notebook>();
+
+            Notebook notebook1 = new Notebook("Predator 15 G9 - 593(NH.Q1ZEU.008) Black", "Acer", 64799);
+            notebooks.Add(notebook1);
+
+            Notebook notebook2 = new Notebook("A1707 MacBook Pro TB Retina 15 Space Gray(MPTT2UA / A)", "Apple", 96099);
+            notebooks.Add(notebook2);
+
+            Notebook notebook3 = new Notebook("ROG G703VI(G703VI - GB001T) Titanium", "Asus", 128099);
+            notebooks.Add(notebook3);
+
+            foreach (var notebook in notebooks)
+            {
+                notebook.Info();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
