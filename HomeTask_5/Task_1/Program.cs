447bcd1492719b8a27e9ae875b215c32313c6272
vchumakov <vchumakov@playtika.com>
2018-04-26 17:47:17 +0300

    add HomeTask_5/Task_1

---
+++ HomeTask_5/Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input 1-t number:");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input 2-d number:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose operation:");
+            string operation = Console.ReadLine();
+            double result = Calculator.Calculate(num1, num2, operation);
+
+            Console.WriteLine("Result:{0}", result);
+       
+            Console.ReadKey();
+        }
+    }
+}
