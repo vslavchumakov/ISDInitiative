8127f4500caca0796f3b39e5b2ac3c175dbe3321
Oleg <olegpetrushov@gmail.com>
2018-04-27 00:00:51 +0300

    заготовочка на 6-ю домашку 3- задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    struct MyStruct
+    {
+        public string change;
+    }
+
+    
+    class Program
+    {
+        /// <summary>
+        /// метод который присваивает полю change 
+        /// экземпляра myClass значение «изменено».
+        /// </summary>
+        /// <param name="myClass"></param>
+        static void ClassTaker(MyClass myClass)
+        {
+            //ToDo realize static function
+        }
+        /// <summary>
+        /// метод который присваивает полю change
+        /// экземпляра myStruct значение «изменено»
+        /// </summary>
+        /// <param name="myStruct"></param>
+        static void StruktTaker(MyStruct myStruct)
+        {
+            //ToDo realize static function
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
