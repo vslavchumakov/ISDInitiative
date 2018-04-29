95e62364d95f73837ec421ef3b5f4ba043a06c7f
Irina <cg.irenecooper@gmail.com>
2018-04-29 22:02:28 +0300

    add HomeTask_10 task_2 wpf app Kulikova

---
+++ HomeTask_10/Task_2_v1_wpf/DocumentWorker.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace readFiles
+{
+    /// <summary>
+    /// класс для работы с документами
+    /// </summary>
+    public static class DocumentWorker
+    {
+        /// <summary>
+        /// фильтр по умолчанию
+        /// </summary>
+        static string _defaultFilter = "All txt files(*txt)|*txt";
+
+        /// <summary>
+        /// расширение по умолчанию
+        /// </summary>
+        static string _defaultExtention = "txt";
+
+        /// <summary>
+        /// начальная директория по умолчанию
+        /// </summary>
+        static string _defaulDirectory = "D:\\";
+
+        /// <summary>
+        /// метод сохраняет документ
+        /// </summary>
+        /// <returns>true- документ сохранён, false- документ не сохранен </returns>
+        public static bool SaveDocument(string text)
+        {
+            SaveFileDialog dialog = new SaveFileDialog//создаём окно сохранить
+            {
+                Filter = _defaultFilter,//фильтр форматов
+                DefaultExt = _defaultExtention,//формат по умолчанию
+                InitialDirectory = _defaulDirectory,//начальная директория
+                CheckPathExists = false//проверка на наличие фаила отключена
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)//если пользователь нажал ок
+            {
+                File.WriteAllText(dialog.FileName, text, Encoding.UTF8);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// метод открывает документ
+        /// </summary>
+        /// <returns></returns>
+        public static string OpenDocument()
+        {
+            OpenFileDialog dialog = new OpenFileDialog //создаём диалог открытия фаила
+            {
+                Filter = "All txt files(*txt)|*txt",  //фильтр форматов
+                InitialDirectory = "D:\\"   //начальная директория
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)//если пользователь нажал ок
+            {
+                return File.ReadAllText(dialog.FileName, Encoding.UTF8);//считываем данные с файла в стороку, заполняем текстбокс
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ HomeTask_10/Task_2_v1_wpf/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace readFiles
+{
+    /// <summary>
+    /// Задание 2. Создайте файл, запишите в него произвольные данные
+    /// и закройте файл. Затем снова откройте этот файл, прочитайте из него данные
+    /// и выведете их на консоль.
+    /// 
+    /// автор: Куликова Ирина
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// подписываемся на событие клика по кнопке открыть фаил
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonOpenClick(object sender, RoutedEventArgs e)
+        {
+            //открываем документ и загружаем данные в текстовую область
+            innerDocumentText.Text = DocumentWorker.OpenDocument();
+        }
+
+        /// <summary>
+        /// подписываемся на событие клика по кнопке сохранить
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonSaveClick(object sender, RoutedEventArgs e)
+        {
+            //сохраняем документ и закрываем приложение
+            if (DocumentWorker.SaveDocument(innerDocumentText.Text))
+            {
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// подписываемся на событие изминения текста в текстовом канторле
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InnerDocumentText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //если в текстовом поле пусто, кнопка сохранить не активна
+            buttonSave.IsEnabled = !String.IsNullOrEmpty(innerDocumentText.Text) ? true : false;
+        }
+    }
+}
