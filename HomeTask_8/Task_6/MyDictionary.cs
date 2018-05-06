96a85810612ce00ca64ec86e4e9ade5023ceaeb2
ShemetViktoriya <shemetvic@ukr.net>
2018-05-07 01:49:47 +0300

    Add HomeTask_8 // Task_6

---
+++ HomeTask_8/Task_6/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿namespace Task_6
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private readonly TKey[] key = null;
+        private readonly TValue[] value = null;
+        private readonly int length = 0;
+
+        /// <summary>
+        /// Read-only property
+        /// </summary>
+        public int Length => length;
+
+        /// <summary>
+        /// User constructor
+        /// </summary>
+        /// <param name="count">Count of elements</param>
+        public MyDictionary(int count)
+        {
+            // Initializes read-only fields in constructor
+            key = new TKey[count];
+            value = new TValue[count];
+            length = count;
+        }
+
+        /// <summary>
+        /// An indexer
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public string this[int index]
+        {
+            get
+            {
+                // Checking the existence of an element in the dictionary
+                if (index >= 0 && index < key.Length)
+                    return key[index] + " - " + value[index];
+                return "Attempt to go beyond the array.";
+            }
+        }
+
+        /// <summary>
+        /// The method of adding an entry to the dictionary
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(int index, TKey addKey, TValue addValue) 
+        {
+            key[index] = addKey;
+            value[index] = addValue;
+        }
+    }
+}
---
+++ HomeTask_8/Task_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Task_6
+{
+    #region Hometask_08_6
+    //
+    // Author: Shemet Viktoriya
+    //
+    // Description: 
+    // Create the class Dictionary <TKey, TValue>. 
+    // Implement in the simplest approximation the possibility of using its instance 
+    // is analogous to an instance of the Dictionary class from the System.Collections.Generic namespace.
+    // The minimum interface required for an instance must include the method of adding element pairs, 
+    // an indexer to retrieve the value of the element at the specified index, 
+    // and a read-only property to obtain the total number of element pairs.
+    //
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instance MyDictionary for 4 elements 
+            var dictionary = new MyDictionary<int, string>(4);
+
+            dictionary.Add(0, 1, "Entry 1");
+            dictionary.Add(1, 2, "Entry 2");
+            dictionary.Add(2, 3, "Entry 3");
+            dictionary.Add(3, 4, "Entry 4");
+
+            // Displays values of dictionary
+            for (int i = 0; i < dictionary.Length; i++)
+                Console.WriteLine(dictionary[i]);
+
+            Console.WriteLine("The third entry in the dictionary:");
+            Console.WriteLine(dictionary[2]); 
+          
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
